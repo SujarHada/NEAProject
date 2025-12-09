bed982fc65c73b50af6d5326e9a7154f4fd496ec
Sameer Paudel <paudelsameer888@gmail.com>
2025-12-09 18:21:01 +0545

    feat: added new units and renamed product name in nepali

--- FE/NEAprojectFE/src/enum/productUnits.ts
+++ FE/NEAprojectFE/src/enum/productUnits.ts
@@ -1,27 +1,92 @@
 export const productUnits = [
     {
         id: 1,
-        name: "KG",
-        value:"kg",
-    },
-    {
-        id: 2,
         name: "Nos",
         value: "nos",
     },
     {
+        id: 2,
+        name: "Set",
+        value: "set",
+    },
+    {
         id: 3,
-        name: "Ltr",
-        value: "ltr",
+        name: "Pair",
+        value: "pair",
     },
     {
         id: 4,
+        name: "Meter",
+        value: "meter",
+    },
+    {
+        id: 5,
+        name: "KG",
+        value: "kg",
+    },
+    {
+        id: 6,
+        name: "Litre",
+        value: "ltr",
+    },
+    {
+        id: 7,
+        name: "Rim",
+        value: "rim",
+    },
+    {
+        id: 8,
+        name: "Pad",
+        value: "pad",
+    },
+    {
+        id: 9,
+        name: "Dozen",
+        value: "dozen",
+    },
+    {
+        id: 10,
+        name: "KMS",
+        value: "kms",
+    },
+    {
+        id: 11,
+        name: "CU. METER",
+        value: "cu_meter",
+    },
+    {
+        id: 12,
         name: "Pcs",
         value: "pcs",
     },
     {
-        id: 5,
-        name: "Set",
-        value: "set",
+        id: 13,
+        name: "Rolls",
+        value: "rolls",
+    },
+    {
+        id: 14,
+        name: "Bottles",
+        value: "bottles",
+    },
+    {
+        id: 15,
+        name: "Packets",
+        value: "packets",
+    },
+    {
+        id: 16,
+        name: "SQ. FT.",
+        value: "sq_ft",
+    },
+    {
+        id: 17,
+        name: "FT.",
+        value: "ft",
+    },
+    {
+        id: 18,
+        name: "Coil",
+        value: "coil",
     }
-]
+];
