f87781dd97c6d5834bace449876222a7fd4223b6
NischalSilwal <nischalsilwalhtd@gmail.com>
2026-01-04 11:34:28 +0545

    feat/change-receiver-id-type: added nepali id card type

--- BE/NEAProjectBE/myapp/models/receiver.py
+++ BE/NEAProjectBE/myapp/models/receiver.py
@@ -4,14 +4,14 @@ from .base import TimeStampedModel
 
 class Receiver(TimeStampedModel):
     class IDCardType(models.TextChoices):
-        NATIONAL_ID = "national_id", "National Identity Card"
-        CITIZENSHIP = "citizenship", "Citizenship Certificate"
-        VOTER_ID = "voter_id", "Voter ID Card"
-        PASSPORT = "passport", "Passport / E-Passport"
-        DRIVERS_LICENSE = "drivers_license", "Driver's License"
-        PAN_CARD = "pan_card", "PAN Card"
-        UNKNOWN = "unknown", "UNKNOWN"
-        EMPLOYEE_ID = "employee_id", "EMPLOYEE ID"
+        NATIONAL_ID = "national_id", "राष्ट्रिय परिचयपत्र"
+        CITIZENSHIP = "citizenship", "नागरिकता प्रमाणपत्र"
+        VOTER_ID = "voter_id", "मतदाता परिचयपत्र"
+        PASSPORT = "passport", "राहदानी / ई–राहदानी"
+        DRIVERS_LICENSE = "drivers_license", "सवारी चालक अनुमति पत्र"
+        PAN_CARD = "pan_card", "स्थायी लेखा नम्बर (प्यान)"
+        UNKNOWN = "unknown", "अज्ञात"
+        EMPLOYEE_ID = "employee_id", "कर्मचारी परिचयपत्र"
 
     name = models.CharField(max_length=255)
     post = models.CharField(max_length=255, default="UNKNOWN")
--- BE/NEAProjectBE/myapp/serializers/receiver.py
+++ BE/NEAProjectBE/myapp/serializers/receiver.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from myapp.models import Receiver
 
 class ReceiverSerializer(serializers.ModelSerializer):
+    id_card_type_display = serializers.CharField(
+        source="get_id_card_type_display",
+        read_only=True
+    )
+
     class Meta:
         model = Receiver
         fields = "__all__"
