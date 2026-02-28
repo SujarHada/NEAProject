dced69ebd9d10924540aee7f81175c60e04a1293
Sameer Paudel <samir.poudel@truenary.com>
2026-02-28 20:42:37 +0545

    fix: make nepali the default language, dont send toke in login api route

--- FE/NEAprojectFE/src/locales/i18n.ts
+++ FE/NEAprojectFE/src/locales/i18n.ts
@@ -4,14 +4,14 @@ import en from "./en/translation.json";
 import np from "./np/translation.json";
 
 i18n.use(initReactI18next).init({
-	resources: {
-		en: { translation: en },
-		np: { translation: np },
-	},
-	fallbackLng: "en",
-	debug: false,
-	interpolation: { escapeValue: false },
-	lng: localStorage.getItem("lang") || undefined,
+  resources: {
+    en: { translation: en },
+    np: { translation: np },
+  },
+  fallbackLng: "np",
+  debug: false,
+  interpolation: { escapeValue: false },
+  lng: localStorage.getItem("lang") || undefined,
 });
 
 export default i18n;
